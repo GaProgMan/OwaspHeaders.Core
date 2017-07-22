9a14415d1f168a3e4c40f1b0ab693ffe936774c3
GaProgMan <jamie@gaprogman.com>
2017-07-22 22:09:09 +0100

    Added HomeController (responds to "/")

---
+++ example/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace example.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        // GET
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return HttpContext.Response.Headers.Select(h => h.ToString()).ToArray();
+        }
+    }
+}

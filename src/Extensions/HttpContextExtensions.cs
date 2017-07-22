4ee0c0e5e78f8047e915f03a23a4d3ec02d33a7d
GaProgMan <jamie@gaprogman.com>
2017-07-22 22:08:23 +0100

    Added TryAddHeader HttpContext extension method

--- src/Extensions/HttpContextExtensions.cs
+++ src/Extensions/HttpContextExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 
 namespace OwaspHeaders.Core.Extensions
 {
@@ -7,6 +8,20 @@ namespace OwaspHeaders.Core.Extensions
         public static bool ResponseContainsHeader(this HttpContext httpContext, string header)
         {
             return httpContext.Response.Headers.ContainsKey(header);
+        }
+
+        public static bool TryAddHeader(this HttpContext httpContext, string headerName, string headerValue)
+        {
+            if (httpContext.ResponseContainsHeader(headerName)) return true;
+            try
+            {
+                httpContext.Response.Headers.Add(headerName, headerValue);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
         }
     }
 }
--- src/SecureHeadersMiddleware.cs
+++ src/SecureHeadersMiddleware.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Abstractions;
 using OwaspHeaders.Core.Extensions;
 using OwaspHeaders.Core.Models;
 
@@ -31,54 +29,56 @@ namespace OwaspHeaders.Core
         /// <returns></returns>
         public async Task Invoke(HttpContext httpContext)
         {
-            if (_config.UseHsts && !httpContext.ResponseContainsHeader(Constants.StrictTransportSecurityHeaderName))
+            if (_config == null)
             {
-                httpContext.Response.Headers.Add(Constants.StrictTransportSecurityHeaderName,
+                throw new ArgumentException($@"Expected an instance of the
+                        {nameof(SecureHeadersMiddlewareConfiguration)} object.");
+            }
+            if (_config.UseHsts)
+            {
+                httpContext.TryAddHeader(Constants.StrictTransportSecurityHeaderName,
                     _config.HstsConfiguration.BuildHeaderValue());
             }
 
-            if (_config.UseHpkp && !httpContext.ResponseContainsHeader(Constants.PublicKeyPinsHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.PublicKeyPinsHeaderName,
-                    _config.HpkpConfiguration.BuildHeaderValue());
-            }
+           if (_config.UseHpkp)
+           {
+               httpContext.TryAddHeader(Constants.PublicKeyPinsHeaderName,
+                   _config.HpkpConfiguration.BuildHeaderValue());
+           }
 
-            if (_config.UseXFrameOptions && !httpContext.ResponseContainsHeader(Constants.XFrameOptionsHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.XFrameOptionsHeaderName,
-                    _config.XFrameOptionsConfiguration.BuildHeaderValue());
-            }
+           if (_config.UseXFrameOptions)
+           {
+               httpContext.TryAddHeader(Constants.XFrameOptionsHeaderName,
+                   _config.XFrameOptionsConfiguration.BuildHeaderValue());
+           }
 
-            if (_config.UseXssProtection && !httpContext.ResponseContainsHeader(Constants.XssProtectionHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.XssProtectionHeaderName,
-                    _config.XssConfiguration.BuildHeaderValue());
-            }
+           if (_config.UseXssProtection)
+           {
+               httpContext.TryAddHeader(Constants.XssProtectionHeaderName,
+                   _config.XssConfiguration.BuildHeaderValue());
+           }
 
-            if (_config.UseXContentTypeOptions &&
-                !httpContext.ResponseContainsHeader(Constants.XContentTypeOptionsHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.XContentTypeOptionsHeaderName, "nosniff");
-            }
+           if (_config.UseXContentTypeOptions)
+           {
+               httpContext.TryAddHeader(Constants.XContentTypeOptionsHeaderName, "nosniff");
+           }
 
-            if (_config.UseContentSecurityPolicy &&
-                !httpContext.ResponseContainsHeader(Constants.ContentSecurityPolicyHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.ContentSecurityPolicyHeaderName,
-                    _config.ContentSecurityPolicyConfiguration.BuildHeaderValue());
-            }
-            if (_config.UsePermittedCrossDomainPolicy &&
-                !httpContext.ResponseContainsHeader(Constants.PermittedCrossDomainPoliciesHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.PermittedCrossDomainPoliciesHeaderName,
-                    _config.PermittedCrossDomainPolicyConfiguration.BuildHeaderValue());
-            }
+           if (_config.UseContentSecurityPolicy)
+           {
+               httpContext.TryAddHeader(Constants.ContentSecurityPolicyHeaderName,
+                   _config.ContentSecurityPolicyConfiguration.BuildHeaderValue());
+           }
+           if (_config.UsePermittedCrossDomainPolicy)
+           {
+               httpContext.TryAddHeader(Constants.PermittedCrossDomainPoliciesHeaderName,
+                   _config.PermittedCrossDomainPolicyConfiguration.BuildHeaderValue());
+           }
 
-            if (_config.UseReferrerPolicy && !httpContext.ResponseContainsHeader(Constants.ReferrerPolicyHeaderName))
-            {
-                httpContext.Response.Headers.Add(Constants.ReferrerPolicyHeaderName,
-                    _config.ReferrerPolicy.BuildHeaderValue());
-            }
+           if (_config.UseReferrerPolicy)
+           {
+               httpContext.TryAddHeader(Constants.ReferrerPolicyHeaderName,
+                   _config.ReferrerPolicy.BuildHeaderValue());
+           }
 
             // Call the next middleware in the chain
             await _next.Invoke(httpContext);

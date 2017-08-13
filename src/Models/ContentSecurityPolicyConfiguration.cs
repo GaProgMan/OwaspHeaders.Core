cc09e3f460a38bd76a0999cec9fc6d6ccd4b337d
Jamie Taylor <seppydude@gmail.com>
2017-08-13 19:36:20 +0100

    Updated the default value for the Content-Security-Policy header's Report Uri value

--- src/Models/ContentSecurityPolicyConfiguration.cs
+++ src/Models/ContentSecurityPolicyConfiguration.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -112,7 +111,7 @@ namespace OwaspHeaders.Core.Models
 
             BlockAllMixedContent = true;
             UpgradeInsecureRequests = true;
-            ReportUri = "https://gaprogman.com";
+            ReportUri = "https://dotnetcore.gaprogman.com";
         }
 
         /// <summary>

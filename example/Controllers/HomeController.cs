327b7a5337a02534a1fb4b5e044ce8f025cdde4d
Jamie <jamie@gaprogman.com>
2022-04-08 20:22:35 +0100

    Replaced example with .NET 6.0 webapi

--- example/Controllers/HomeController.cs
+++ example/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
-namespace example.Controllers
+namespace example.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController : ControllerBase
 {
-    [Route("/")]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        // GET
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return HttpContext.Response.Headers.Select(h => h.ToString()).ToArray();
-        }
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "/")]
+    public IEnumerable<string> Get()
+    {
+        return HttpContext.Response.Headers.Select(h => h.ToString()).ToArray();
     }
 }
--- example/Program.cs
+++ example/Program.cs
@@ -1,21 +1,30 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+using OwaspHeaders.Core.Enums;
+using OwaspHeaders.Core.Extensions;
 
-namespace example
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        
-        public static void Main(string[] args)
-        {
-            var webHost = BuildWebHost(args);
-
-            webHost.Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseSecureHeadersMiddleware(SecureHeadersMiddlewareExtensions.BuildDefaultConfiguration());
+
+app.MapControllers();
+
+app.Run();

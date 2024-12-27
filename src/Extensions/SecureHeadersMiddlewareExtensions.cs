54523d9b00c12ef52458c758a3075ea770ffbd6d
Jamie Taylor <seppydude@gmail.com>
2024-12-27 03:08:41 +0000

    Leveraged COOP models

--- src/Extensions/SecureHeadersMiddlewareExtensions.cs
+++ src/Extensions/SecureHeadersMiddlewareExtensions.cs
@@ -33,6 +33,7 @@ public static class SecureHeadersMiddlewareExtensions
             .UseCacheControl()
             .UseXssProtection()
             .UseCrossOriginResourcePolicy()
+            .UseCrossOriginOpenerPolicy()
             .SetUrlsToIgnore(urlIgnoreList)
             .Build();
     }

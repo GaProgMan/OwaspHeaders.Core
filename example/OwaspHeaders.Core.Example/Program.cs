40c74a6b1ae05c98fe2a635f2d3e20edb405ab91
Jamie Taylor <jamie.taylor@rjj-software.co.uk>
2025-03-29 16:22:11 +0000

    PR 170: Added Reporting-Endpoints to example

--- example/OwaspHeaders.Core.Example/Program.cs
+++ example/OwaspHeaders.Core.Example/Program.cs
@@ -20,8 +20,13 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+// Example of using both a relative URL where headers should not be preset and the [EXPERIMENTAL] Reporting-Endpoints header
 var listOfUrlsToIgnore = new List<string> { "/skipthis" };
-app.UseSecureHeadersMiddleware(urlIgnoreList: listOfUrlsToIgnore);
+var reportingEndpoints =
+    new Dictionary<string, Uri> { { "standard", new Uri("https://localhost:5000/reporting-endpoint") } };
+var secureHeadersMiddlewareConfig = SecureHeadersMiddlewareBuilder.CreateBuilder()
+    .UseReportingEndpointsPolicy(reportingEndpoints).SetUrlsToIgnore(listOfUrlsToIgnore).Build();
+app.UseSecureHeadersMiddleware(secureHeadersMiddlewareConfig);
 
 app.MapControllers();
 
